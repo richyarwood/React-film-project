0b8fe91d17ab62bc3f6e65476914a8e2f9207fae
Richard Yarwood <richyarwood@gmail.com>
2019-04-21 12:48:44 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+import './style.scss'
+
+class App extends React.Component {
+  render() {
+    return(
+      <h1>Hello</h1>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
